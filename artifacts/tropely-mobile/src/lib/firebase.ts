9a82005426779c4b229a4dc500ba64d8c854f68a
ashleykat21 <browneyesroux@gmail.com>
2026-05-16 18:50:06 -0500

    fix: use initializeAuth with AsyncStorage persistence for React Native

--- artifacts/tropely-mobile/src/lib/firebase.ts
+++ artifacts/tropely-mobile/src/lib/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCg7GB1Ors1MKTj9S3yo0bgiPXctpd66Yw",
@@ -8,8 +9,10 @@ const firebaseConfig = {
   storageBucket: "tropely-ba3a0.firebasestorage.app",
   messagingSenderId: "162706231594",
   appId: "1:162706231594:web:3922a002d72c4716338e53",
-  measurementId: "G-CLM27BSB31",
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
